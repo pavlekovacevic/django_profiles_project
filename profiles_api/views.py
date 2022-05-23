bdf2fc2edb4b17e58876add70603eefebe06461a
pavlekovacevic <kovacevicpavle14@gmail.com>
2022-05-23 19:28:46 +0200

    views added and urlpatterns

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test Api View"""
+
+    def get(self, request, format=None):
+        """Returns a list of ApiView features"""
+        an_apiview = [
+            "Uses http methods as functions(get, post, patch, put, delete)",
+            "Is similar to a traditional Django view",
+            "Gives you the most control over the app logic",
+            "Is mapped manually to URLs"
+        ]
+
+        return Response({'message':'Hello', 'an_apiview':an_apiview})
